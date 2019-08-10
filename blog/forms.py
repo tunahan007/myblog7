445f6c64132d675c69947f7ff6eccf9729159971
tunahan007 <hilmi_demirkaya@yahoo.de>
2019-08-10 23:39:02 +0200

    adding everything hopefully

---
+++ blog/forms.py
@@ -0,0 +1,26 @@
+from django import forms
+from .models import Comment,Contact
+
+
+class EmailPostForm(forms.Form):
+    name = forms.CharField(max_length=25)
+    email = forms.EmailField()
+    to = forms.EmailField()
+    comments = forms.CharField(required=False,
+                               widget=forms.Textarea)
+
+
+class CommentForm(forms.ModelForm):
+    class Meta:
+        model = Comment
+        fields = ('name', 'email', 'body')
+
+
+class SearchForm(forms.Form):
+    query = forms.CharField()
+
+
+class ContactForm(forms.ModelForm):
+    class Meta:
+        model = Contact
+        fields = ('first_name', 'last_name', 'email', 'message')
